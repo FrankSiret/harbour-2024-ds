4e4d19cc104581144e554e014791a8f8f478bdac
FrankSiret <frank.siret@gmail.com>
2024-06-13 20:48:15 +0200

    move project to new location - remove old ds project

--- src/main/docker/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Starting..."
-exec java -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "$@"
--- src/main/docker/jib/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Starting..."
-exec java -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.hs.distributedsystem.DistributedsystemApplication"  "$@"
